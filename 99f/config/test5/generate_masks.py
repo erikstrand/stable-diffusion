4618604ad998cabf27b92776c25044dc73f33266
Erik Strand <erik.strand@cba.mit.edu>
2022-09-30 13:37:59 +0000

    Add bounds to mask generating script

--- 99f/config/test5/generate_masks.py
+++ 99f/config/test5/generate_masks.py
@@ -1,13 +1,31 @@
 from config_reader import load_config
 from pathlib import Path
 from masks import save_mask_image
+import argparse
 
 width = 640
 height = 360
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--first_frame',
+        '-f',
+        default=None,
+        type=int,
+        help='create masks starting at this frame'
+    )
+    parser.add_argument(
+        '--last_frame',
+        '-g',
+        default=None,
+        type=int,
+        help='create masks ending after this frame'
+    )
+    args = parser.parse_args()
+
     # load the dream schedule
-    ds = load_config("config_05.json")
+    ds = load_config("config.json")
 
     indir = Path(ds.indir)
     maskdir = Path(ds.maskdir)
@@ -17,6 +35,9 @@ if __name__ == "__main__":
     next_keyframe = ds.schedule[0]
     next_keyframe_idx = 1
 
+    first_frame = args.first_frame if args.first_frame is not None else next_keyframe.frame
+    last_frame = args.last_frame
+
     masks = []
     while next_keyframe_idx < len(ds.schedule):
         prev_keyframe = next_keyframe
@@ -24,7 +45,15 @@ if __name__ == "__main__":
         next_keyframe_idx += 1
         interp_len = float(next_keyframe.frame - prev_keyframe.frame)
 
+        # Keep going if the first frame doesn't appear between these keyframes.
+        if next_keyframe.frame <= first_frame:
+            continue
+
         for frame_idx in range(prev_keyframe.frame, next_keyframe.frame, ds.stride):
+            # Stop if we've past the last frame
+            if last_frame is not None and frame_idx > last_frame:
+                break
+
             t = float(frame_idx - prev_keyframe.frame) / interp_len
             interp_masks = []
             for prev_mask, next_mask in zip(prev_keyframe.masks, next_keyframe.masks):
