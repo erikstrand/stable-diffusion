eb825e9a7151563bde178eff18d84385845dffff
Erik Strand <erik.strand@cba.mit.edu>
2022-09-30 14:03:59 +0000

    More tests

--- 99f/config/test5/make_videos.sh
+++ 99f/config/test5/make_videos.sh
@@ -1,7 +1,1 @@
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_01/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_01.mp4
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_02/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_02.mp4
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_03/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_03.mp4
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_04/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_04.mp4
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_05/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_05.mp4
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_05b/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_05b.mp4
-ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test4/clip_05c/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test4/videos/clip_05c.mp4
+ffmpeg -r 12.5 -f image2 -s 640x320 -i ../../../outputs/test5/clip_00/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../../outputs/test5/videos/clip_00.mp4
