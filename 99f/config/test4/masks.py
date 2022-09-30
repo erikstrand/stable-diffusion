455b190b1b2600111cbaa5f376c582a56f9329b3
Erik Strand <erik.strand@cba.mit.edu>
2022-09-29 23:33:28 -0400

    Allow multiple circle per mask

--- 99f/config/test4/masks.py
+++ 99f/config/test4/masks.py
@@ -7,7 +7,7 @@ def generate_black_rectangle_array(width, height):
     return np.zeros((width, height, 3), dtype='uint8')
 
 
-def generate_mask_array(width, height, center, radius):
+def generate_mask_array(width, height, circles):
     width = float(width)
     height = float(height)
 
@@ -21,15 +21,25 @@ def generate_mask_array(width, height, center, radius):
     #        print(f"{i}, {j}: {coords[i, j]}")
     # dist[i, j] = distance from (i, j) to center
 
-    # Rescale center and radius
-    center = np.array([width, height]) * center
-    radius = height * radius
+    result = np.full(coords.shape[0:2], 255, dtype='uint8')
 
-    dist = np.linalg.norm(coords - center, axis=2)
-    # mask as True/False
-    mask = dist > radius
-    # mask as 255/0
-    return (255 * mask).astype('uint8')
+    for circle in circles:
+        # Rescale center and radius.
+        center = circle[0]
+        center = np.array([width, height]) * center
+        radius = circle[1]
+        radius = height * radius
+
+        # Generate the mask.
+        dist = np.linalg.norm(coords - center, axis=2)
+        # mask as True/False
+        mask = dist > radius
+        # mask as 255/0 (255 means opaque, 0 means transparent)
+        mask = (255 * mask).astype('uint8')
+
+        result = np.minimum(result, mask)
+
+    return result
 
 
 def array_to_image(array):
@@ -38,25 +48,33 @@ def array_to_image(array):
     return Image.fromarray(array, format)
 
 
-def generate_mask_image(width, height, center, radius):
+def generate_mask_image(width, height, circles):
     rgb_array = generate_black_rectangle_array(width, height)
-    mask_array = generate_mask_array(width, height, center, radius)
+    mask_array = generate_mask_array(width, height, circles)
     mask_array = np.expand_dims(mask_array, axis=2)
     image_data = np.concatenate((rgb_array, mask_array), axis=2)
     return array_to_image(image_data)
 
 
-def save_mask_image(width, height, center, radius, filename):
-    image = generate_mask_image(width, height, center, radius)
+def save_mask_image(width, height, circles, filename):
+    image = generate_mask_image(width, height, circles)
     image.save(filename)
 
 
 if __name__ == "__main__":
+    # in pixels
     width = 640
     height = 320
+
     # relative to width and height
     center = np.array([0.5, 0.5])
+
     # relative to height
     radius = 0.25
 
-    save_mask_image(width, height, center, radius, 'mask.png')
+    circles = [
+        (center, radius),
+        (np.array([0.7, 0.8]), 0.2)
+    ]
+
+    save_mask_image(width, height, circles, 'mask.png')
