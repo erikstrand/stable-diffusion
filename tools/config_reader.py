9f6407d5d871bc96a6c0ba2d95e0bbf9adea3e47
Erik Strand <erik.strand@cba.mit.edu>
2022-10-04 14:51:45 -0400

    Add 2D animation a la deforum to config files

--- tools/config_reader.py
+++ tools/config_reader.py
@@ -20,19 +20,32 @@ class Mask:
         return f"Mask: center {self.center[0]}, {self.center[1]}, radius {self.radius}"
 
 
-class KeyFrame:
-    __slots__ = ["frame", "prompt", "seed", "scale", "strength", "masks"]
+class Animation2D:
+    __slots__ = ["zoom", "translation", "rotation"]
 
-    def __init__(self, frame, prompt, seed, scale, strength, masks):
+    def __init__(self, zoom, translation, rotation):
+        self.zoom = float(zoom)
+        self.translation = (float(translation[0]), float(translation[1]))
+        self.rotation = float(rotation)
+
+
+class KeyFrame:
+    __slots__ = ["frame", "prompt", "seed", "scale", "strength", "masks", "animation"]
+
+    def __init__(self, frame, prompt, seed, scale, strength, masks, animation):
         self.frame = frame
         self.prompt = prompt
         self.seed = seed
         self.scale = scale
         self.strength = strength
+
         assert(isinstance(masks, list))
         for mask in masks:
             assert(isinstance(mask, Mask))
         self.masks = masks
+
+        assert(animation is None or isinstance(animation, Animation2D))
+        self.animation = animation
 
     @classmethod
     def from_dict(cls, dict):
@@ -47,6 +60,14 @@ class KeyFrame:
             dict["masks"] = []
         else:
             dict["masks"] = [Mask(**mask) for mask in dict["masks"]]
+        if "animation" not in dict or dict["animation"] == "none":
+            dict["animation"] = None
+        else:
+            dict["animation"] = Animation2D(
+                dict["animation"]["zoom"],
+                dict["animation"]["translate"],
+                dict["animation"]["rotate"],
+            )
 
         return KeyFrame(
             dict["frame"],
@@ -55,6 +76,7 @@ class KeyFrame:
             dict["scale"],
             dict["strength"],
             dict["masks"],
+            dict["animation"],
         )
 
     @classmethod
@@ -82,6 +104,19 @@ class KeyFrame:
         else:
             dict["masks"] = [Mask(**mask) for mask in dict["masks"]]
 
+        if "animation" not in dict or dict["animation"] == "same":
+            dict["animation"] = prev_keyframe.animation
+        elif dict["animation"] == "none":
+            print(dict["animation"])
+            dict["animation"] = None
+        else:
+            print(dict["animation"])
+            dict["animation"] = Animation2D(
+                dict["animation"]["zoom"],
+                dict["animation"]["translate"],
+                dict["animation"]["rotate"],
+            )
+
         return KeyFrame(
             dict["frame"],
             dict["prompt"],
@@ -89,6 +124,7 @@ class KeyFrame:
             dict["scale"],
             dict["strength"],
             dict["masks"],
+            dict["animation"],
         )
 
     def __str__(self):
