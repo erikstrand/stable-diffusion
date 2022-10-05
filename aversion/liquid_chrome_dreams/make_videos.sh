0ab83a59b8bb51be6a6a86dde05b0f2fefe1f76a
Erik Strand <erik.strand@cba.mit.edu>
2022-10-05 09:07:22 -0400

    Update liquid_chrome_dreams/make_videos.sh

--- aversion/liquid_chrome_dreams/make_videos.sh
+++ aversion/liquid_chrome_dreams/make_videos.sh
@@ -1,1 +1,1 @@
-ffmpeg -r 10.0 -f image2 -s 512x512 -i ../../outputs/liquid_chrome_dreams/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../outputs/liquid_chrome_dreams/clip_00.mp4
+ffmpeg -r 10.0 -f image2 -s 512x512 -i ../../outputs/liquid_chrome_dreams/%06d.0.png -vcodec libx264 -crf 10 -pix_fmt yuv420p ../../outputs/liquid_chrome_dreams/liquid_chrome.mp4
