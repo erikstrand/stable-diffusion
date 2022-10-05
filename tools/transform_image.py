1176aa15a07897061b5f77adffb4f7dfe77bd29d
Erik Strand <erik.strand@cba.mit.edu>
2022-10-05 13:32:25 -0400

    Clean things up

--- tools/transform_image.py
+++ tools/transform_image.py
@@ -11,59 +11,27 @@ def array_to_image(array):
     format = 'RGB' if array.shape[2] == 3 else 'RGBA'
     return Image.fromarray(array, format)
 
-def transform(image_array):
-    angle = 10.0
-    zoom = 1.2
-    translation = (-10.0, 50.0)
-
+def transform(image_array, zoom=1.0, angle=0.0, translation=(0.0, 0.0)):
     center = (0.5 * (image_array.shape[1] - 1), 0.5 * (image_array.shape[0] - 1))
-
-    trans_mat = np.float32([[1, 0, translation[1]], [0, 1, translation[0]]])
+    trans_mat = np.float32([
+        [1.0, 0.0, translation[1]],
+        [0.0, 1.0, translation[0]],
+        [0.0, 0.0, 1.0]
+    ])
     rot_mat = cv.getRotationMatrix2D(center, angle, zoom)
-    print(trans_mat.shape)
-    print(rot_mat.shape)
-    trans_mat = np.vstack([trans_mat, [0,0,1]])
-    rot_mat = np.vstack([rot_mat, [0,0,1]])
-    print(trans_mat.shape)
-    print(rot_mat.shape)
+    rot_mat = np.vstack([rot_mat, [0.0, 0.0, 1.0]])
     xform = np.matmul(rot_mat, trans_mat)
-
-    return cv.warpPerspective(image_array, xform, (image_array.shape[1], image_array.shape[0]))
-    #return cv.warpPerspective(
-    #    image_array,
-    #    rot_mat,
-    #    (image_array.shape[1], image_array.shape[0]),
-    #    borderMode=cv.BORDER_REPLICATE
-    #)
+    return cv.warpPerspective(
+        image_array,
+        xform,
+        (image_array.shape[1], image_array.shape[0]),
+        borderMode=cv.BORDER_REPLICATE
+    )
 
 if __name__ == "__main__":
-    """
-    img = cv.imread("niku_1.jpeg")
-    print(type(img))
-    print(img.shape)
-    print(img.dtype)
-    print(img)
-
-    rows, cols = img.shape[0:2]
-    # cols-1 and rows-1 are the coordinate limits.
-    M = cv.getRotationMatrix2D(((cols-1)/2.0, (rows-1)/2.0), 45.0, 1)
-    dst = cv.warpAffine(img,M,(cols,rows))
-    cv.imwrite("niku_1_cv.png", dst)
-    """
-
-
     # Any image will do.
     image = Image.open("niku_1.jpeg")
-
-    rgb_array = image_to_array(image)
-    print(rgb_array.shape)
-    print(rgb_array.dtype)
-    print(rgb_array)
-
-    rgb_array = transform(rgb_array)
-    print(rgb_array.shape)
-    print(rgb_array.dtype)
-    print(rgb_array)
-
-    image = array_to_image(rgb_array)
+    array = image_to_array(image)
+    array = transform(array, 1.2, -10.0, (15.0, 0.0))
+    image = array_to_image(array)
     image.save("niku_modified.png")
