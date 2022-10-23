be4ba6f5b4a2007003ff87c74e18e5ddda08869c
Erik Strand <erik.strand@cba.mit.edu>
2022-10-23 16:28:51 +0000

    Don't overwrite command file when generating masks

--- tools/dream_compiler.py
+++ tools/dream_compiler.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         "--start_at",
         type=int,
         help="The first frame to render (1-indexed).",
-        default=0
+        default=1
     )
     parser.add_argument(
         "-e",
@@ -83,41 +83,53 @@ if __name__ == "__main__":
     # Build the dream schedule.
     schedule = DreamSchedule.from_file(args.config_file)
 
-    # Generate commands.
+    # Initialize the dream state.
     if args.end_at is None:
-        args.end_at = schedule.keyframes[-1].frame
+        # Minus two since we don't output the last keyframe, and we're 1-indexed.
+        args.end_at = schedule.keyframes[-1].frame - 2
     dream_state = DreamState(schedule)
     frames = []
-    with open(args.outfile, "w") as outfile:
-        if args.commands:
-            # We have to set our prompts regardless of start and end frames.
-            prompt_command = schedule.prompt_command()
-            outfile.write(prompt_command + '\n')
+    outfile = open(args.outfile, "w") if args.commands else None
 
-        # Render the frames.
-        while not dream_state.done() and dream_state.frame_idx <= args.end_at:
-            # We always generate the command so that random seeds are the same no matter where we start.
-            command = dream_state.get_command()
-            frames.append(dream_state.output_path())
+    # If we're writing commands, we always write the prompts.
+    if args.commands:
+        prompt_command = schedule.prompt_command()
+        outfile.write(prompt_command + '\n')
 
-            if args.start_at <= dream_state.frame_idx:
-                if args.commands:
-                    outfile.write(command + '\n')
-                if args.masks:
-                    masks = dream_state.get_masks()
-                    if len(masks) > 0:
-                        image_file = dream_state.input_image_path()
-                        mask_file = dream_state.mask_path()
-                        print(f"generating mask for frame {dream_state.frame_idx} ({mask_file})")
-                        save_mask_image(
-                            schedule.width,
-                            schedule.height,
-                            masks,
-                            image_file,
-                            mask_file
-                        )
+    # Iterate over all frames.
+    n_commands = 0
+    while not dream_state.done() and dream_state.frame_idx <= args.end_at:
+        # We generate the command even if we're not writing it anywhere so that random seeds are the
+        # same no matter where we start.
+        command = dream_state.get_command()
+        frames.append(dream_state.output_path())
 
-            dream_state.advance_frame()
+        if args.start_at <= dream_state.frame_idx:
+            # Write the command if requested.
+            if args.commands:
+                outfile.write(command + '\n')
+                n_commands += 1
+
+            # Generate the mask if requested.
+            if args.masks:
+                masks = dream_state.get_masks()
+                if len(masks) > 0:
+                    image_file = dream_state.input_image_path()
+                    mask_file = dream_state.mask_path()
+                    print(f"generating mask for frame {dream_state.frame_idx} ({mask_file})")
+                    save_mask_image(
+                        schedule.width,
+                        schedule.height,
+                        masks,
+                        image_file,
+                        mask_file
+                    )
+
+        dream_state.advance_frame()
+
+    # Print a summary if we wrote commands.
+    if args.commands:
+        print(f"Wrote {n_commands} commands to {args.outfile} (frames {args.start_at} to {args.end_at}).")
 
     if args.video:
         # Write the list of frames to a file.
