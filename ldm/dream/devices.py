0886955dd462a91e94d3da9c199d042715d31941
Serge <serge.vasylechko@tch.harvard.edu>
2023-01-10 22:40:08 -0500

    adding a new file called devices.py that returns cuda status. Required for running inferrence on NVIDIA A40

---
+++ ldm/dream/devices.py
@@ -0,0 +1,8 @@
+import torch
+
+def choose_torch_device():
+    if torch.cuda.is_available():
+        return 'cuda'
+    elif torch.backends.mps.is_available():
+        return 'mps'
+    return 'cpu'
